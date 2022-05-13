90b790adad55fea4b9265e64cf2a8e81c75bdf83
nwf03 <nwf.aloufi@gmail.com>
2022-05-13 00:49:57 -0400

    implemented rpc methods and moved location field from node to rpc

--- src/rpc.rs
+++ src/rpc.rs
@@ -2,51 +2,96 @@ use std::{
     collections::HashMap,
     net::UdpSocket,
     sync::{Arc, Mutex},
+    thread,
+    time::Duration,
 };
 
 use crossbeam_channel::{Receiver, Sender};
 
 use crate::{
-    message::{Message, Response},
-    protocol::Protocol,
+    message::{Message, MessageType, Response},
+    node::Location,
 };
 
-pub struct RPC {
-    socket: UdpSocket,
+const TIMEOUT: Duration = Duration::new(5, 0);
+pub const CONCCURENT_REQS: usize = 3;
+
+#[derive(Debug, Clone)]
+pub struct Rpc {
+    socket: Arc<UdpSocket>,
+    pub location: Location,
     pending: Arc<Mutex<HashMap<String, Sender<Option<Response>>>>>,
 }
 
-impl RPC {
-    pub fn new(addr: String) -> Self {
-        let socket = UdpSocket::bind(addr).unwrap();
+impl Rpc {
+    pub fn new(location: Location) -> Self {
+        let socket = UdpSocket::bind(location.addr()).unwrap();
         Self {
-            socket,
+            location,
+            socket: Arc::new(socket),
             pending: Arc::new(Mutex::new(HashMap::new())),
         }
     }
+
+    pub fn listen(rpc: Rpc, sender: Sender<Message>) {
+        thread::spawn(move || loop {
+            let mut buff = [0u8; 4096];
+            let (n, _) = rpc.socket.recv_from(&mut buff).unwrap();
+            let msg: Message = bincode::deserialize(&buff[..n]).unwrap();
+            match msg.msg {
+                MessageType::Terminate => todo!(),
+                MessageType::Request(_) => {
+                    sender.send(msg.clone()).unwrap();
+                }
+                MessageType::Response(res) => {
+                    let ps = Arc::clone(&rpc.pending);
+                    let mut pending = ps.lock().unwrap();
+                    let p_sender = pending.get(&msg.id);
+
+                    if let Some(psender) = p_sender {
+                        psender.send(Some(res)).expect("failed to send to channel");
+                        pending.remove(&msg.id);
+                    }
+                }
+            }
+        });
+    }
+
     pub fn send_request(&self, msg: Message) -> Receiver<Option<Response>> {
-        todo!()
-    }
-}
+        let (tx, rx) = crossbeam_channel::bounded(1);
+        let pending = Arc::clone(&self.pending);
+        let mut ps = pending.lock().unwrap();
+        ps.insert(msg.id.clone(), tx.clone());
+        drop(ps);
+        self.send_msg(msg.clone())
+            .expect("failed to send msg to node");
 
-impl Protocol for RPC {
-    fn ping(&self, node_addr: String) -> anyhow::Result<()> {
-        todo!()
+        let pending = Arc::clone(&self.pending);
+        thread::spawn(move || {
+            thread::sleep(TIMEOUT);
+            if tx.send(None).is_ok() {
+                let mut pending = pending.lock().unwrap();
+                pending.remove(&msg.id);
+            }
+        });
+
+        rx
     }
 
-    fn store(key: String, val: String) -> anyhow::Result<()> {
-        todo!()
+    pub fn send_msg(&self, msg: Message) -> anyhow::Result<()> {
+        let encoded_msg = bincode::serialize(&msg).expect("unable to serialize message");
+        if self
+            .socket
+            .send_to(encoded_msg.as_slice(), msg.dist.addr())
+            .is_ok()
+        {
+            anyhow::Ok(())
+        } else {
+            Err(anyhow::anyhow!("failed to send msg"))
+        }
     }
 
-    fn find_node(id: crate::guid::GUID) -> Vec<crate::node::Location> {
-        todo!()
-    }
-
-    fn find_value(key: String) -> Option<String> {
-        todo!()
-    }
-
-    fn get_value(key: String) -> Option<String> {
-        todo!()
+    pub fn get_location(&self) -> Location {
+        self.location.clone()
     }
 }
