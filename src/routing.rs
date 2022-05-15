715054bd4eac6c387de0b048ba684334871154d5
nwf03 <nwf.aloufi@gmail.com>
2022-05-14 22:43:24 -0400

    fixed nearest_nodes_to_id method and added insert and remove methods

--- src/routing.rs
+++ src/routing.rs
@@ -1,11 +1,10 @@
 use crate::{
     guid::{Distance, GUID, GUID_LEN},
-    node::{self, Location, BUCKET_LEN},
+    node::{Location, BUCKET_LEN},
 };
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct RoutingTable(pub Vec<Bucket>);
-
 const BUCKET_SIZE: usize = 20;
 impl RoutingTable {
     pub fn new() -> Self {
@@ -20,28 +19,66 @@ impl RoutingTable {
 
     pub fn nearest_nodes_to_id(&self, node_id: &GUID, id: &GUID) -> Vec<Location> {
         let mut idx = Bucket::find_index(node_id, id);
-        let mut bucket = &self.0[idx];
         let mut closest_nodes = Vec::with_capacity(BUCKET_SIZE);
         let mut idx_copy = idx;
 
-        while closest_nodes.len() != BUCKET_SIZE {
-            bucket = &self.0[idx];
+        while closest_nodes.len() != BUCKET_SIZE && idx < self.0.len() - 1 {
+            idx += 1;
+            let bucket = &self.0[idx];
             for node in &bucket.0 {
                 closest_nodes.push(node.clone());
+                if closest_nodes.len() == BUCKET_SIZE {
+                    break;
+                }
             }
-            if idx < self.0.len() - 1 {
-                idx += 1
-            } else {
-                idx_copy -= 1;
-                idx = idx_copy
+        }
+
+        idx = idx_copy;
+
+        while closest_nodes.len() != BUCKET_SIZE && idx > 0 {
+            let bucket = &self.0[idx];
+            for node in &bucket.0 {
+                closest_nodes.push(node.clone());
+                if closest_nodes.len() == BUCKET_SIZE {
+                    break;
+                }
             }
+            idx -= 1
         }
 
         closest_nodes
     }
+
+    pub fn insert(&mut self, id: GUID, node_location: Location) {
+        let bucket_idx = Bucket::find_index(&id, &node_location.id);
+        let bucket = &mut self.0[bucket_idx];
+        if !bucket.is_full() {
+            bucket.insert(node_location);
+        }
+    }
+
+    pub fn print_buckets(&self) {
+        for b in &self.0 {
+            if b.0.len() > 0 {
+                for l in &b.0 {
+                    println!("{:?}", l);
+                }
+            }
+        }
+        println!("------------------------------");
+    }
+
+    pub fn remove(&mut self, id: &GUID, node_id: &GUID) {
+        let bucket_idx = Bucket::find_index(id, node_id);
+        let bucket = &mut self.0[bucket_idx];
+        let node_idx = bucket.0.iter().position(|r| r.id == node_id.clone());
+        if let Some(idx) = node_idx {
+            bucket.0.remove(idx);
+        }
+    }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Bucket(pub Vec<Location>);
 
 impl Bucket {
@@ -49,15 +86,15 @@ impl Bucket {
         Self(Vec::with_capacity(BUCKET_SIZE))
     }
 
-    fn find_index(id: &GUID, node_id: &GUID) -> usize {
-        let dist = node_id.distance_from(&id);
+    pub fn find_index(id: &GUID, node_id: &GUID) -> usize {
+        let dist = node_id.distance_from(id);
         let diff = dist.distance_from(&node_id.0);
 
         for i in 0..GUID_LEN {
             for j in (0..8).rev() {
                 let bit = diff[i] & (1 << j);
                 if bit != 0 {
-                    return 8 * i - j;
+                    return 8 * i + j;
                 }
             }
         }
@@ -65,12 +102,15 @@ impl Bucket {
         GUID_LEN * 8 - 1
     }
 
-    fn is_empty(&self) -> bool {
-        self.0.is_empty()
+    fn insert(&mut self, node: Location) {
+        let node_idx = self.0.iter().position(|l| l.id == node.id);
+        if let Some(idx) = node_idx {
+            self.0.remove(idx);
+        }
+        self.0.push(node);
     }
 
-    fn len(&self) -> usize {
-        self.0.len()
+    fn is_full(&self) -> bool {
+        self.0.len() >= BUCKET_SIZE
     }
 }
-
